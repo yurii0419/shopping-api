ad08cca74de3f63122002f89db971b5678d18344
yurii0419 <yurii.babii.work9521@ukr.net>
2024-03-12 16:11:00 +0800

    Login UI

--- app/Livewire/Pages/Auth/Login.php
+++ app/Livewire/Pages/Auth/Login.php
@@ -2,12 +2,31 @@
 
 namespace App\Livewire\Pages\Auth;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 
 class Login extends Component
 {
-    public function render()
-    {
-        return view('livewire.pages.auth.login');
-    }
+
+  
+
+  public $email = '';
+  public $password = '';
+
+  public function LogIn()
+  {
+    $credentials = $this->validate([
+      'email' => ['required', 'email'],
+      'password' => 'required'
+    ]);
+
+    
+
+  }
+
+  public function render()
+  {
+    return view('livewire.pages.auth.login');
+  }
 }
--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -1,1 +1,55 @@
-<h1>Login Page</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  @vite('resources/css/app.css')
+</head>
+
+<body>
+  <!-- component -->
+  <div class="flex min-h-screen items-center justify-content-end bg-white p-12" style="background-image:url('{{ asset('assets/img/loginBg.png') }}'); background-repeat:no-repeat; background-position:center;">
+    <form action="">
+      <div class="max-w-sm">
+        <div class="rounded-[calc(1.5rem-1px)] bg-transarent px-10 p-12">
+          <div>
+            <p class="text-2xl text-center tracking-wide font-black">Hello, We're happy to see you here again!</p>
+          </div>
+
+          <div class="mt-8 space-y-8">
+            <form action="{{ route('login') }}" 
+            wire:submit.prevent="submit">
+            @csrf
+            <div class="space-y-6">
+              <input class="w-full bg-white text-gray-600 rounded-md border border-gray-300 px-3 py-2 text-sm placeholder-gray-600 invalid:border-red-500" 
+
+              placeholder="Email or Username" 
+              type="username" 
+              name="username" 
+              id="username" />
+              <a href="" class="LoginPhone underline">Login in with Phone Number</a>
+
+              <input class="w-full bg-white text-gray-600 rounded-md border border-gray-300 px-3 py-2 text-sm placeholder-gray-600 invalid:border-red-500" 
+              placeholder="Password" type="password" name="password" id="password" />
+              <a href="" class="ForgotPassword underline">Forgot Password</a>
+            </div>
+
+            <button 
+            type="button"
+            wire:click="LogIn"
+            class="h-9 px-3 w-full bg-orange-600 rounded-md text-white">
+              Signin
+            </button>
+          </form>
+
+            
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+</body>
+
+</html>
