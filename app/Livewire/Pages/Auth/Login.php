7ef5e7851f5f83ceac9fe1b957d91c620333abd3
yurii0419 <yurii.babii.work9521@ukr.net>
2024-03-13 12:07:27 +0800

    added changes to helper and to do

--- app/Livewire/Pages/Auth/Login.php
+++ app/Livewire/Pages/Auth/Login.php
@@ -42,38 +42,47 @@ class Login extends Component
             ]);
             session()->flash('error', 'Looks like the credentials you entered are not found');
         }else{
-            $this->phone_otp = 1;
             // TO DO: Create logic for OTP
-            // send OTP
+            $this->phone_otp = 1;
+            $otp_gen = otp_generator();
+            User::where('email', '=', $this->email)
+            ->update([
+                'otp_code' => $otp_gen
+            ]);
+            // send OTP with helper function movider_service();
             // get user data base on $this->email
             // $this->phone_number = $user_data->phone_number
             // get user OTP Code base on result for database query
             // match input OTP to database OTP
 
-            auth()->login($user, $this->remember_me);
-            User::where('id', '=', auth()->user()->id)
-            ->update([
-                'lastlogin' => Carbon::now()
-            ]);
-            $log_name = 'User login';
-            $log_desc = 'User successfull login using email '.$this->email;
-            LogService::userLog($log_name,$log_desc);
-            $this->dispatch('success_login_swal', [
-                'title' => 'Success',
-                'timer'=>3000,
-                'icon'=>'success',
-                'toast'=>false,
-                'position'=>'center',
-                'text' => 'You have logged in successfully. Welcome back! '.auth()->user()->user_firstname.' '.auth()->user()->user_lastname
-            ]);
+            // auth()->login($user, $this->remember_me);
+            // User::where('id', '=', auth()->user()->id)
+            // ->update([
+            //     'lastlogin' => Carbon::now()
+            // ]);
+            // $log_name = 'User login';
+            // $log_desc = 'User successfull login using email '.$this->email;
+            // LogService::userLog($log_name,$log_desc);
+            // $this->dispatch('success_login_swal', [
+            //     'title' => 'Success',
+            //     'timer'=>3000,
+            //     'icon'=>'success',
+            //     'toast'=>false,
+            //     'position'=>'center',
+            //     'text' => 'You have logged in successfully. Welcome back! '.auth()->user()->user_firstname.' '.auth()->user()->user_lastname
+            // ]);
 
-            session()->flash('message', 'You have logged in successfully. Welcome back! '.auth()->user()->user_firstname.' '.auth()->user()->user_lastname);
+            // session()->flash('message', 'You have logged in successfully. Welcome back! '.auth()->user()->user_firstname.' '.auth()->user()->user_lastname);
         }
     }
 
     public function emailSend(){
         $this->phone_otp = 0;
         $otp_gen = otp_generator();
+        User::where('email', '=', $this->email)
+        ->update([
+            'otp_code' => $otp_gen
+        ]);
         Mail::to($this->email)->send(new SendOtpViaMail($otp_gen));
         $this->email_otp = 1;
     }
