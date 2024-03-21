da0e574843a6733ff7f70b84ea9f9b4fa85ad7f4
yurii0419 <yurii.babii.work9521@ukr.net>
2024-03-21 19:29:09 +0800

    Updated Products-info backend

--- app/Livewire/Pages/Auth/ProductsInfo.php
+++ app/Livewire/Pages/Auth/ProductsInfo.php
@@ -3,11 +3,26 @@
 namespace App\Livewire\Pages\Auth;
 
 use Livewire\Component;
+use App\Models\Product;
 
 class ProductsInfo extends Component
 {
+    public Product $products;
+
+    // Remove the public $keywords; line if you no longer need it
+
+    public function mount(Product $products)
+    {
+        $this->products = $products->load('user');
+    }
+
     public function render()
     {
-        return view('livewire..pages.auth.products-info');
+        // If you have the cast set up in your model, you can just pass the attribute.
+        return view('livewire.pages.auth.products-info', [
+            'product' => $this->products,
+            'keywords' => $this->products->keyword, // Assuming the 'keyword' attribute is already an array
+            'user' => $this->products->user
+        ]);
     }
 }
